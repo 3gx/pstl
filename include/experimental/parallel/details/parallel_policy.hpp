62925c24464aaee15e8bd3f678b4791a2b346831
Evghenii Gaburov <egaburov@dds.nl>
2015-10-27 16:39:48 -0700

    killed detials namespace, just add underscore

--- include/experimental/parallel/details/parallel_policy.hpp
+++ include/experimental/parallel/details/parallel_policy.hpp
@@ -17,14 +17,14 @@ inline namespace v1    {
 class parallel_execution_policy
 {
     template<class InputIterator, class Function>
-    friend void dispatch(const details::for_each&, const parallel_execution_policy &par, 
+    friend void dispatch(const __for_each&, const parallel_execution_policy &par, 
                          InputIterator first, InputIterator last, Function f) 
     {
         PARSPACE::for_each(first, last, f);
     }
 
     template<class RandomIt>
-    friend void dispatch(const details::sort&, const parallel_execution_policy &seq,
+    friend void dispatch(const __sort&, const parallel_execution_policy &seq,
                          RandomIt first, RandomIt last)
     {
         PARSPACE::sort(first, last);
