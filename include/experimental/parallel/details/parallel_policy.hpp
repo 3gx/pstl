7a9ed351c921eddb5955481536e33c1edfd1f66d
Evghenii Gaburov <egaburov@dds.nl>
2015-10-27 15:48:32 -0700

    moved detail->details

--- include/experimental/parallel/details/parallel_policy.hpp
+++ include/experimental/parallel/details/parallel_policy.hpp
@@ -12,14 +12,14 @@ inline namespace v1    {
 class parallel_execution_policy
 {
     template<class InputIterator, class Function>
-    friend void dispatch(const detail::for_each&, const parallel_execution_policy &par, 
+    friend void dispatch(const details::for_each&, const parallel_execution_policy &par, 
                          InputIterator first, InputIterator last, Function f) 
     {
         __gnu_parallel::for_each(first, last, f);
     }
 
     template<class RandomIt>
-    friend void dispatch(const detail::sort&, const parallel_execution_policy &seq,
+    friend void dispatch(const details::sort&, const parallel_execution_policy &seq,
                          RandomIt first, RandomIt last)
     {
         __gnu_parallel::sort(first, last);
@@ -43,14 +43,14 @@ inline namespace v1    {
 class parallel_execution_policy
 {
     template<class InputIterator, class Function>
-    friend void dispatch(const detail::for_each&, const parallel_execution_policy &par, 
+    friend void dispatch(const details::for_each&, const parallel_execution_policy &par, 
                          InputIterator first, InputIterator last, Function f) 
     {
         std::for_each(first, last, f);
     }
     
     template<class RandomIt>
-    friend void dispatch(const detail::sort&, const parallel_execution_policy &seq,
+    friend void dispatch(const details::sort&, const parallel_execution_policy &seq,
                          RandomIt first, RandomIt last)
     {
         std::sort(first, last);
--- include/experimental/parallel/details/sequential_policy.hpp
+++ include/experimental/parallel/details/sequential_policy.hpp
@@ -12,14 +12,14 @@ inline namespace v1    {
 class sequential_execution_policy
 {
     template<class InputIterator, class Function>
-    friend void dispatch(const detail::for_each&, const sequential_execution_policy &seq, 
+    friend void dispatch(const details::for_each&, const sequential_execution_policy &seq, 
                          InputIterator first, InputIterator last, Function f) 
     {
         std::for_each(first, last, f);
     }
 
     template<class RandomIt>
-    friend void dispatch(const detail::sort&, const sequential_execution_policy &seq,
+    friend void dispatch(const details::sort&, const sequential_execution_policy &seq,
                          RandomIt first, RandomIt last)
     {
         std::sort(first, last);
